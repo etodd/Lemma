752e12e5ba6106aad6624af945cd63e7ef9ad9c8
Evan Todd <et1337@gmail.com>
2014-03-19 09:26:06 -0400

    Allow Unicode in web templates. Add YouTube policy.

--- Web/compile.py
+++ Web/compile.py
@@ -21,4 +21,4 @@ env = jinja2.Environment(loader = jinja2.FileSystemLoader(INPUT_DIR))
 
 for f in (f for f in os.listdir(INPUT_DIR) if os.path.isfile(f) and not IGNORE(f) and os.path.splitext(f)[1] == EXTENSION):
 	with open(os.path.join(OUTPUT_DIR, os.path.splitext(f)[0]), 'w') as newFile:
-		newFile.write(env.get_template(f).render())
+		newFile.write(env.get_template(f).render().encode('utf8'))
