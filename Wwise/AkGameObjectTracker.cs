4d75172003aa8fde00fe70eddde09630ea6ebf0f
Evan Todd <et1337@gmail.com>
2014-04-19 15:03:21 -0400

    AkGameObjectTracker improvements.

--- Wwise/AkGameObjectTracker.cs
+++ Wwise/AkGameObjectTracker.cs
@@ -9,53 +9,69 @@ using ComponentBind;
 
 //Add this script on a Game Object that will emit sounds and that will move during gameplay.
 //For more information, see the Wwise SDK doc in the section AK::SoundEngine::SetObjectPosition
-public class AkGameObjectTracker : AkGameObject, IUpdateableComponent
+public class AkGameObjectTracker : AkGameObject
 {    
 	private Vector3 pos;
 	private Vector3 forward;
 
 	private bool hasMoved;
-	private bool initial = true;
 
 	public bool HasMovedInLastFrame()
 	{
-		return this.hasMoved;
+		bool result = this.hasMoved;
+		this.hasMoved = false;
+		return result;
 	}
 
 	public Property<Matrix> Matrix = new Property<Matrix>();
 
 	public static void Attach(Entity result, Property<Matrix> property = null)
 	{
-		AkGameObjectTracker tracker = result.GetOrCreate<AkGameObjectTracker>();
-		if (property == null)
-			property = result.Get<Transform>().Matrix;
-		tracker.Add(new Binding<Matrix>(tracker.Matrix, property));
+		AkGameObjectTracker tracker = result.Get<AkGameObjectTracker>();
+		if (tracker == null)
+		{
+			tracker = new AkGameObjectTracker();
+			result.Add(tracker);
+			if (property == null)
+				property = result.Get<Transform>().Matrix;
+			tracker.Add(new Binding<Matrix>(tracker.Matrix, property));
+		}
 	}
 
 	public static void Attach(Entity result, Property<Vector3> property)
 	{
-		AkGameObjectTracker tracker = result.GetOrCreate<AkGameObjectTracker>();
-		tracker.Add(new Binding<Matrix, Vector3>(tracker.Matrix, x => Microsoft.Xna.Framework.Matrix.CreateTranslation(x), property));
+		AkGameObjectTracker tracker = result.Get<AkGameObjectTracker>();
+		if (tracker == null)
+		{
+			tracker = new AkGameObjectTracker();
+			result.Add(tracker);
+			tracker.Add(new Binding<Matrix, Vector3>(tracker.Matrix, x => Microsoft.Xna.Framework.Matrix.CreateTranslation(x), property));
+		}
 	}
-	
-	public void Update(float dt)
+
+	public override void InitializeProperties()
+	{
+		base.InitializeProperties();
+		this.Add(new NotifyBinding(this.Update, this.Matrix));
+		this.Update();
+	}
+
+	public void Update()
 	{
 		Matrix m = this.Matrix;
 		Vector3 pos = m.Translation;
 		Vector3 forward = m.Forward;
-		if (!this.initial && this.pos == pos && this.forward == forward)
+		if (this.pos == pos && this.forward == forward)
 		{
 			this.hasMoved = false;
 			return;
 		}
 
-		this.initial = false;
-
 		this.pos = pos;
 		this.forward = forward;    
 		this.hasMoved = true;
 
-		//Update position
+		// Update position
 		AkSoundEngine.SetObjectPosition(
 			this.Entity, 
 			pos.X, 
@@ -65,6 +81,6 @@ public class AkGameObjectTracker : AkGameObject, IUpdateableComponent
 			forward.Y, 
 			forward.Z);
 
-		//Update Object-Listener distance RTPC, if needed. (TODO)
-	}   
+		// Update Object-Listener distance RTPC, if needed. (TODO)
+	}
 }
