f28dd853cd040fbc1b910f75bf3548dc9c2cbaa4
Evan Todd <et1337@gmail.com>
2014-05-22 10:17:56 -0400

    Update Game content project template.

--- Lemma/Template/test.cs
+++ Lemma/Template/test.cs
@@ -1,26 +1,47 @@
-GameMain.Config settings = ((GameMain)main).Settings;
+using System;
+using ComponentBind;
+using Lemma.GameScripts;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lemma.Util;
+using Lemma.Components;
+using Lemma.Factories;
 
-Entity playerData = Factory.Get<PlayerDataFactory>().Instance(main);
-
-Entity player = null;
-script.Add(new CommandBinding<Entity>(((GameMain)main).PlayerSpawned, delegate(Entity p)
+namespace Lemma.GameScripts
 {
-	player = p;
-}));
-
-Updater playerDamager = new Updater
-{
-	delegate(float dt)
+	public class test : ScriptBase
 	{
-		if (player != null && player.Active)
+		public static Entity script;
+		public static void Run()
 		{
-			Player p = player.Get<Player>();
-			float y = p.Transform.Value.Translation.Y;
-			if (y < -40.0f || (y < -20.0f && p.LinearVelocity.Value.Y < -40.0f))
-				player.Delete.Execute();
+			GameMain.Config settings = ((GameMain)main).Settings;
+
+			Entity playerData = Factory.Get<PlayerDataFactory>().Instance(main);
+
+			Entity player = null;
+			script.Add(new CommandBinding<Entity>(((GameMain)main).PlayerSpawned, delegate(Entity p)
+			{
+				player = p;
+			}));
+
+			Updater playerDamager = new Updater
+			{
+				delegate(float dt)
+				{
+					if (player != null && player.Active)
+					{
+						Player p = player.Get<Player>();
+						float y = p.Transform.Value.Translation.Y;
+						if (y < -40.0f || (y < -20.0f && p.LinearVelocity.Value.Y < -40.0f))
+							player.Delete.Execute();
+					}
+					else
+						player = null;
+				}
+			};
+			script.Add(playerDamager);
 		}
-		else
-			player = null;
 	}
-};
-script.Add(playerDamager);
+}
