9c9079cdd99e07a6cf931b4daef77e5728ad1676
Evan Todd <et1337@gmail.com>
2015-02-14 20:18:05 -0500

    Update menu water settings. Bump version number.

--- Lemma/Main.cs
+++ Lemma/Main.cs
@@ -79,8 +79,8 @@ namespace Lemma
 		}
 
 		public const int ConfigVersion = 9;
-		public const int MapVersion = 952;
-		public const int Build = 952;
+		public const int MapVersion = 959;
+		public const int Build = 959;
 
 		public static bool AllowEditingGameMaps;
 
