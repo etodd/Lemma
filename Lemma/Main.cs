f5780ba5357318d51059994b4a01ab4dd708b242
Evan Todd <et1337@gmail.com>
2015-05-18 22:23:32 -0400

    Bump version number.

--- Lemma/Main.cs
+++ Lemma/Main.cs
@@ -93,7 +93,7 @@ namespace Lemma
 
 		public const int ConfigVersion = 9;
 		public const int MapVersion = 1027;
-		public const int Build = 1064;
+		public const int Build = 1070;
 
 		public class Config
 		{
